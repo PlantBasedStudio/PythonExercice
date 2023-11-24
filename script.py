053bc29cca210732c9ec22cce0ef457a1f2d0e30
Damien <ultrarushgame@gmail.com>
2023-11-24 19:30:55 +0100

    Base exercices Python

---
+++ script.py
@@ -0,0 +1,25 @@
+# Test
+livre = "Moby dick"
+print(livre)
+
+# Exercice 1, F-string et variables
+nom = "Damien"
+age = 27
+print(f"Je m'appelle {nom} et j'ai {age} ans.")
+age = int(age) + 10
+print(f"Je m'appelle {nom} et j'ai {age} ans.")
+
+# Exercice 2, variables bool, float, int
+duree_episode = 20
+pourcentage = 3.2
+succes = True
+sentence = "Oui bonjour, c'est josé à l'appareil"
+print(type(age))
+
+# Exercice 3, reconnaître les types de variables
+nom = "Damien"
+age = 27
+taille = 1.70
+est_etudiant = True
+print(nom, age, taille, est_etudiant)
+print(type(nom), type(age), type(taille), type(est_etudiant))
